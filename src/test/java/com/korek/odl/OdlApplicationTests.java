fa2329597c60ed0bc3426ce58f26a21ec066f0f4
Kamil Korenkiewicz <kkorenkiewicz1@gmail.com>
2018-08-26 00:21:41 +0200

    Initial commit

---
+++ src/test/java/com/korek/odl/OdlApplicationTests.java
@@ -0,0 +1,16 @@
+package com.korek.odl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OdlApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
